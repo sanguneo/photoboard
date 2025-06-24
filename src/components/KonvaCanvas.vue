4583e11bf64a5cdefa7d55de071661d27a3ca8e8
seungheon1029 <seungheon1029@gmail.com>
2025-06-24 15:58:45 +0900

    ✨ 이미지 회전, 크롭 기능

--- src/components/KonvaCanvas.vue
+++ src/components/KonvaCanvas.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import type Konva from 'konva';
+
 interface KonvaCanvasProps {
   imageSrc?: string;
   width?: number;
@@ -7,38 +9,74 @@ interface KonvaCanvasProps {
 }
 
 const props = defineProps<KonvaCanvasProps>();
-const isDragging = ref(false);
-
-// 이미지 로딩
-const image = ref<HTMLImageElement | null>(null);
-
-const loadImage = async () => {
-  if (!props.imageSrc) return;
-
-  const img = new Image();
-  img.onload = () => {
-    image.value = img;
-  };
-
-  img.src = props.imageSrc;
-};
-
-// 이미지 변경 감지
-watch(() => props.imageSrc, loadImage, { immediate: true });
-
-// Stage 크기 설정
-const stageSize = computed(() => ({
+const getStageSize = computed(() => ({
   width: props.width || window.innerWidth,
   height: props.height || window.innerHeight,
 }));
 
-// 텍스트 그룹의 초기 위치
-const groupPos = ref({
-  x: 100,
-  y: 100,
-});
+const isDragging = ref(false);
+const backgroundImage = ref<HTMLImageElement | null>(null);
+const transformerRef = ref();
+const crop = ref({ x: 0, y: 0, width: getStageSize.value.width - 1, height: getStageSize.value.height - 1 });
+const cropRect = ref();
+const stageRef = ref();
+const rotation = ref(0);
 
-// 드래그 이벤트 핸들러
+const imageConfig = computed(() => ({
+  image: backgroundImage.value,
+  width: getStageSize.value.width,
+  height: getStageSize.value.height,
+  rotation: rotation.value,
+  offset: {
+    x: getStageSize.value.width / 2,
+    y: getStageSize.value.height / 2,
+  },
+  x: getStageSize.value.width / 2,
+  y: getStageSize.value.height / 2,
+}));
+
+const cropRectConfig = computed(() => ({
+  x: crop.value.x,
+  y: crop.value.y,
+  width: crop.value.width,
+  height: crop.value.height,
+  draggable: true,
+}));
+
+const transformerConfig = {
+  rotateEnabled: false,
+  anchorCornerRadius: 10,
+  anchorSize: 6,
+};
+
+const groupConfig = computed(() => ({
+  x: getStageSize.value.width / 2,
+  y: getStageSize.value.height - 150,
+  draggable: true,
+}));
+
+const rectConfig = computed(() => ({
+  width: 200,
+  height: props.photoInfoList.length * 20 + 30,
+  fill: '#fff',
+  stroke: '#000',
+  strokeWidth: 0.1,
+  cornerRadius: 10,
+}));
+
+const textConfig = (index: number, title: string, content: string) => {
+  return {
+    text: `${title}: ${content}`,
+    x: 20,
+    y: 20 + index * 20,
+    fontSize: 14,
+    fontFamily: 'Arial',
+    width: 200,
+    align: 'left',
+    fill: '#000',
+  };
+};
+
 const handleDragStart = () => {
   isDragging.value = true;
 };
@@ -46,59 +84,97 @@ const handleDragStart = () => {
 const handleDragEnd = () => {
   isDragging.value = false;
 };
+
+const loadImage = async () => {
+  if (!props.imageSrc) return;
+
+  const img = new Image();
+  img.onload = () => {
+    backgroundImage.value = img;
+  };
+
+  img.src = props.imageSrc;
+};
+
+const rotateImage = () => {
+  rotation.value += 90;
+  if (rotation.value >= 360) {
+    rotation.value = 0;
+  }
+};
+
+const cropImage = () => {
+  const stage = stageRef.value?.getNode();
+  if (!stage) return;
+
+  const { x, y, width, height } = crop.value;
+  const dataURL = stage.toDataURL({
+    x,
+    y,
+    width: width,
+    height: height,
+    mimeType: 'image/jpeg',
+    quality: 1,
+    pixelRatio: 1,
+  });
+
+  return dataURL;
+};
+
+const onDragMove = (e: { target: Konva.Node }) => {
+  const node = e.target;
+  crop.value.x = node.x();
+  crop.value.y = node.y();
+
+  // 이동된 위치
+  let newX = node.x();
+  let newY = node.y();
+
+  // 범위 제한
+  newX = Math.max(0, Math.min(newX, getStageSize.value.width - node.width()));
+  newY = Math.max(0, Math.min(newY, getStageSize.value.height - node.height()));
+
+  node.x(newX);
+  node.y(newY);
+
+  crop.value.x = newX;
+  crop.value.y = newY;
+};
+
+const onTransformEnd = () => {
+  const node = cropRect.value.getNode();
+
+  // Konva에서 transform 적용 후에는 scale이 적용되므로 이를 반영해 계산
+  const scaleX = node.scaleX();
+  const scaleY = node.scaleY();
+
+  crop.value.x = node.x();
+  crop.value.y = node.y();
+  crop.value.width = node.width() * scaleX;
+  crop.value.height = node.height() * scaleY;
+
+  node.scaleX(1);
+  node.scaleY(1);
+};
+
+watch(() => props.imageSrc, loadImage, { immediate: true });
+onMounted(() => {
+  transformerRef.value?.getNode().nodes([cropRect.value?.getNode()]);
+});
+defineExpose({ cropImage, rotateImage });
 </script>
 
 <template>
-  <v-stage :config="stageSize">
+  <v-stage ref="stageRef" :config="getStageSize">
     <v-layer>
-      <!-- 이미지 -->
-      <v-image
-        v-if="image"
-        :config="{
-          image: image,
-          width: stageSize.width,
-          height: stageSize.height,
-        }"
-      />
+      <v-image v-if="backgroundImage" :config="imageConfig" />
 
-      <!-- 텍스트 그룹 -->
-      <v-group
-        v-if="photoInfoList.length > 0"
-        :config="{
-          x: groupPos.x,
-          y: groupPos.y,
-          draggable: true,
-        }"
-        @dragstart="handleDragStart"
-        @dragend="handleDragEnd"
-      >
-        <!-- 배경 사각형 -->
-        <v-rect
-          :config="{
-            width: 200,
-            height: photoInfoList?.length * 20 + 30,
-            fill: '#fff',
-            stroke: '#000',
-            strokeWidth: 0.1,
-            cornerRadius: 10,
-          }"
-        />
+      <v-rect ref="cropRect" :config="cropRectConfig" @dragmove="onDragMove" @transformend="onTransformEnd" />
+      <v-transformer ref="transformerRef" :config="transformerConfig" />
 
-        <!-- 텍스트 -->
-        <v-text
-          v-for="(text, index) in photoInfoList"
-          :key="index"
-          :config="{
-            text: `${text.title}: ${text.content}`,
-            x: 20,
-            y: 20 + index * 20,
-            fontSize: 14,
-            fontFamily: 'Arial',
-            width: 200,
-            align: 'left',
-            fill: '#000',
-          }"
-        />
+      <v-group v-if="photoInfoList.length > 0" :config="groupConfig" @dragstart="handleDragStart" @dragend="handleDragEnd">
+        <v-rect :config="rectConfig" />
+        <v-text v-for="(text, index) in photoInfoList" :key="index" :config="textConfig(index, text.title, text.content)" />
       </v-group>
     </v-layer>
   </v-stage>
--- src/pages/konva.vue
+++ src/pages/konva.vue
@@ -9,11 +9,17 @@ interface IInputList {
 }
 
 const inputList = ref<IInputList[]>([
-  { title: '단지명', value: '', placeholder: '단지명' },
-  { title: '위치', value: '', placeholder: '위치' },
-  { title: '내용', value: '', placeholder: '내용' },
-  { title: '날짜', value: '', placeholder: '날짜' },
+  { title: '단지명', value: '단지명', placeholder: '단지명' },
+  { title: '위치', value: '위치', placeholder: '위치' },
+  { title: '내용', value: '내용', placeholder: '내용' },
+  { title: '날짜', value: '날짜', placeholder: '날짜' },
 ]);
+
+const konvaCanvas = ref();
+const croppedImage = ref<string | null>(null);
+const cropImage = () => {
+  croppedImage.value = konvaCanvas.value.cropImage();
+};
 
 const photoInfoList = ref<{ title: string; content: string }[]>([]);
 
@@ -44,11 +50,21 @@ const update = () => {
     <div v-for="input in inputList" :key="input.title" class="input-group">
       <input v-model="input.value" :placeholder="input.placeholder" maxlength="30" />
     </div>
-    <input type="file" accept="image/*" @change="handleFile" />
-    <button @click="update">완료</button>
+    <input type="file" accept="image/*" class="file-input" @change="handleFile" />
+    <div class="button-group">
+      <button @click="update">캔버스 업데이트</button>
+      <button @click="konvaCanvas.rotateImage">회전</button>
+      <button @click="cropImage">자르기</button>
+    </div>
   </div>
-
-  <KonvaCanvas :width="500" :height="500" :image-src="imageSrc" :photo-info-list="photoInfoList" />
+  <div class="canvas-container">
+    <h3>캔버스</h3>
+    <KonvaCanvas ref="konvaCanvas" :width="500" :height="500" :image-src="imageSrc" :photo-info-list="photoInfoList" />
+    <h3>자른 이미지</h3>
+    <div class="cropped-image">
+      <img v-if="croppedImage" :src="croppedImage" alt="" />
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -59,8 +75,9 @@ const update = () => {
   margin-bottom: 20px;
 
   .input-group {
+    width: 500px;
     input {
-      width: 300px;
+      width: 100%;
       height: 40px;
       padding: 8px;
       border: 1px solid #ccc;
@@ -68,8 +85,13 @@ const update = () => {
     }
   }
 
+  .button-group {
+    display: flex;
+    gap: 10px;
+  }
+
   button {
-    width: 100px;
+    width: 150px;
     height: 40px;
     background-color: #4caf50;
     color: white;
@@ -82,5 +104,26 @@ const update = () => {
       background-color: #45a049;
     }
   }
+}
+
+.canvas-container {
+  display: flex;
+  gap: 20px;
+}
+
+.cropped-image {
+  width: 500px;
+  height: 500px;
+  border: 1px solid #ccc;
+}
+
+.file-input {
+  width: 500px;
+  margin-bottom: 20px;
+  padding: 10px 20px;
+  background: #fff;
+  border: none;
+  border-radius: 8px;
+  box-shadow: 0 2px 5px rgb(0 0 0 / 0.1);
 }
 </style>
