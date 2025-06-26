42755a72d884786ab72018ed7037696f61215675
sanguneo <sknah0319@gmail.com>
2025-06-26 14:29:24 +0900

    :sparkles: 네이티브 기능확인 33

--- src/pages/wifi.vue
+++ src/pages/wifi.vue
@@ -2,6 +2,29 @@
 
 const { log, isOnline, networkType, data, data2 } = usePageVisibility();
 
+const stat = ref();
+
+function getNetworkType() {
+  const connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+
+  if (!connection) {
+    return 'unknown'; // 브라우저가 지원하지 않는 경우
+  }
+
+  // connection.type 은 deprecated, 대신 effectiveType 사용
+  const type = connection.effectiveType;
+
+  if (type.includes('wifi')) return 'wifi';
+  if (type.includes('cellular') || type.includes('3g') || type.includes('4g') || type.includes('5g')) return 'cellular';
+
+  return type; // 'ethernet', 'slow-2g', etc.
+}
+
+const onClickbutton = () => {
+  stat.value = getNetworkType();
+};
+
+
 
 onMounted(() => {
   console.log('isConnect', isOnline.value);
@@ -17,6 +40,9 @@ onMounted(() => {
     <h1>networkType : {{ networkType }}</h1>
     <h1>sample data : {{ data }}</h1>
     <h1>sample data2222222222222 : {{ data2 }}</h1>
+
+    <h1><button @click="onClickbutton">클릭</button></h1>
+    <h1>stat : {{ stat }}</h1>
   </main>
 </template>
 <style lang="scss" scoped>
