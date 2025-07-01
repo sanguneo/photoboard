b974f9b087e9d8a8b8a30e6bb23594346867c017
sanguneo <sknah0319@gmail.com>
2025-07-01 09:14:01 +0900

    :sparkles: 네이티브 기능확인 44

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,12 @@ export default defineNuxtConfig({
   ssr: false,
   app: {
     head: {
-      meta: [],
+      meta: [
+        { name: 'viewport', content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no' },
+      ],
       link: [],
       style: [],
-      script: [
-        // { src: '/modules/filer.min.js' },
-      ],
+      script: [],
       title: 'Xp사진보드판',
     },
   },
