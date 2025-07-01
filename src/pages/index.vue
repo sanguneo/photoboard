bd535f176275eadf293ed9cb3c0cdca61d400b6e
sanguneo <sknah0319@gmail.com>
2025-07-01 10:21:11 +0900

    :sparkles: 네이티브 기능확인 55

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,6 +5,10 @@
     <NuxtLink to="/idb">IDB</NuxtLink>
     <br><br>
     <NuxtLink to="/native">네이티브 기능 확인</NuxtLink>
+    <br><br>
+    <NuxtLink to="/wifi">브라우저 기본 api</NuxtLink>
+    <br><br>
+    <a href="tel:01092292790">전화테스트</a>
   </main>
 </template>
 <style lang="scss" scoped>
