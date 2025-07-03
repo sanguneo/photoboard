bf4d7b91e76f1353cf9a27bdc5a2ee68838cb242
sanguneo <sknah0319@gmail.com>
2025-07-03 14:40:45 +0900

    :sparkles: 네이티브 기능확인 99

--- src/pages/native.vue
+++ src/pages/native.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import heic2any from 'heic2any';
 
 const wifiStatus = ref<string>('⏳ 상태 확인 전');
 const preview = ref<string>();
 
-const onChangeFile = (event: Event) => {
-  const file = (event.target as HTMLInputElement)?.files?.[0];
+const onChangeFile = async (event: Event) => {
+  let file = (event.target as HTMLInputElement)?.files?.[0] as File;
   if (file) {
+    if (file.name.includes('.heic')) {
+      file = (await heic2any({ blob: file, toType: 'image/jpeg', quality: 1 })) as File;
+    }
     const reader = new FileReader();
     reader.onload = function (e) {
       preview.value = (e.target as FileReader).result as string;
